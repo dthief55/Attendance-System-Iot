9d55703e72e7a929ed89e8f52f7aea9f193584cc
Gabriel Fernando <fernandogabriel662@gmail.com>
2024-06-05 14:04:31 +0700

    Success Integrate Layout and Components
    Success to linking each pages with layout, and integrate layout with all components (top-nav, side-nav, footer).

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard Kehadiran Siswa</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="{{ asset ('css/style.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+
+        <x-top-nav></x-top-nav>
+
+        <div id="layoutSidenav">
+            <x-side-nav></x-side-nav>
+
+            <div id="layoutSidenav_content">
+                {{ $slot }}
+
+                <x-footer></x-footer>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset("js/scripts.js") }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset("js/chart-area-demo.js") }}"></script>
+        <script src="{{ asset("js/chart-bar-demo.js") }}"></script>
+        <script src="{{ asset("js/chart-pie-demo.js") }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset("js/datatables-simple-demo.js") }}"></script>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,201 +1,112 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Dashboard Kehadiran Siswa</title>
-        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="{{ asset ('css/style.css') }}" rel="stylesheet" />
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="/">UAS IoT</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="/"><i class="fas fa-bars"></i></button>
-            <!-- Navbar Search-->
-            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">                      
-                <!-- <div class="input-group">
-                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-                </div> -->
-            </form>
-            <!-- Navbar-->
-            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/">Settings</a></li>
-                        <li><a class="dropdown-item" href="/">Activity Log</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="/login">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="/">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Dashboard
-                            </a>
-                            <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="/charts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Grafik
-                            </a>
-                            <a class="nav-link" href="/tables">
-                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                                Tabel
-                            </a>
-                        </div>
+<x-layout>
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Dashboard</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Kehadiran Siswa</li>
+            </ol>
+            <div class="row">
+                <div class="col-xl-3 col-md-6">
+                    <div class="card bg-primary text-white mb-4">
+                        <div class="card-body">Tombol 1</div>
                     </div>
-                    <!-- <div class="sb-sidenav-footer">
-                        <div class="small">Logged in as:</div>
-                        Start Bootstrap
-                    </div> -->
-                </nav>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="card bg-warning text-white mb-4">
+                        <div class="card-body">Tombol 2</div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="card bg-success text-white mb-4">
+                        <div class="card-body">Tombol 3</div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="card bg-danger text-white mb-4">
+                        <div class="card-body">Tombol 4</div>
+                    </div>
+                </div>
             </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Dashboard</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Kehadiran Siswa</li>
-                        </ol>
-                        <div class="row">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Tombol 1</div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Tombol 2</div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Tombol 3</div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Tombol 4</div>
-                                </div>
-                            </div>
+            
+            <!-- Area Chart Example -->
+            <div class="row">
+                <div class="col-xl-6">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-chart-area me-1"></i>
+                            Contoh Grafik Area
                         </div>
-                        
-                        <!-- Area Chart Example -->
-                        <div class="row">
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-area me-1"></i>
-                                        Contoh Grafik Area
-                                    </div>
-                                    <div class="card-body"><canvas id="grafikTotal" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
-                            <!-- Bar Chart Example -->
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-bar me-1"></i>
-                                        Contoh Grafik Bar
-                                    </div>
-                                    <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
-                                </div>
-                            </div>
+                        <div class="card-body"><canvas id="grafikTotal" width="100%" height="40"></canvas></div>
+                    </div>
+                </div>
+                <!-- Bar Chart Example -->
+                <div class="col-xl-6">
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-chart-bar me-1"></i>
+                            Contoh Grafik Bar
                         </div>
+                        <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+                    </div>
+                </div>
+            </div>
 
-                        <div class="card mb-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                Contoh Data Tabel
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>No.</th>
-                                            <th>Nama</th>
-                                            <th>NIM</th>
-                                            <th>Kelas</th>
-                                            <th>Kehadiran</th>
-                                            <th>Jenis Kelamin</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>No.</th>
-                                            <th>Nama</th>
-                                            <th>NIM</th>
-                                            <th>Kelas</th>
-                                            <th>Kehadiran</th>
-                                            <th>Jenis Kelamin</th>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        <tr>
-                                            <td>1.</td>
-                                            <td>Valentino Wahyu Pratama</td>
-                                            <td>3332210021</td>
-                                            <td>A</td>
-                                            <td>Hadir</td>
-                                            <td>Pria</td>
-                                        </tr>
-                                        <tr>
-                                            <td>2.</td>
-                                            <td>Gabriel Fernando</td>
-                                            <td>3332210047</td>
-                                            <td>B</td>
-                                            <td>Tidak Hadir</td>
-                                            <td>Pria</td>
-                                        </tr>
-                                        <tr>
-                                            <td>3.</td>
-                                            <td>Hadid Tamir</td>
-                                            <td>3332210060</td>
-                                            <td>C</td>
-                                            <td>Hadir</td>
-                                            <td>Pria</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; UAS IoT Kelompok-7</div>
-                            <div>
-                                <a href="/">Privacy Policy</a>
-                                &middot;
-                                <a href="/">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    Contoh Data Tabel
+                </div>
+                <div class="card-body">
+                    <table id="datatablesSimple">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Nama</th>
+                                <th>NIM</th>
+                                <th>Kelas</th>
+                                <th>Kehadiran</th>
+                                <th>Jenis Kelamin</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>No.</th>
+                                <th>Nama</th>
+                                <th>NIM</th>
+                                <th>Kelas</th>
+                                <th>Kehadiran</th>
+                                <th>Jenis Kelamin</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <tr>
+                                <td>1.</td>
+                                <td>Valentino Wahyu Pratama</td>
+                                <td>3332210021</td>
+                                <td>A</td>
+                                <td>Hadir</td>
+                                <td>Pria</td>
+                            </tr>
+                            <tr>
+                                <td>2.</td>
+                                <td>Gabriel Fernando</td>
+                                <td>3332210047</td>
+                                <td>B</td>
+                                <td>Tidak Hadir</td>
+                                <td>Pria</td>
+                            </tr>
+                            <tr>
+                                <td>3.</td>
+                                <td>Hadid Tamir</td>
+                                <td>3332210060</td>
+                                <td>C</td>
+                                <td>Hadir</td>
+                                <td>Pria</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset("js/scripts.js") }}"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset("js/chart-area-demo.js") }}"></script>
-        <script src="{{ asset("js/chart-bar-demo.js") }}"></script>
-        <script src="{{ asset("js/chart-pie-demo.js") }}"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset("js/datatables-simple-demo.js") }}"></script>
-    </body>
-</html>
+    </main>
+</x-layout>
